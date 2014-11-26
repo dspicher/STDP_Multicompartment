46a89d35bde3bd0c4759be41a7679efa474c6cd2
dominik <dominik@bccn-berlin.de>
2014-11-26 15:17:09 +0100

    added accumulator logic to get simulation results

--- simulation.py
+++ simulation.py
@@ -4,7 +4,7 @@ from IPython import embed
 from util import get_default
 from model import get_spike_currents, phi, phi_prime, urb_senn_rhs
 
-def run(sim, spiker, neuron=None, phi_params=None, learn=None, **kwargs):
+def run(sim, spiker, spiker_dendr, accumulator, neuron=None, phi_params=None, learn=None, **kwargs):
 
     np.random.seed(kwargs.get('seed',0))
 
@@ -28,52 +28,72 @@ def run(sim, spiker, neuron=None, phi_params=None, learn=None, **kwargs):
     n_steps = int((t_end-t_start)/dt)+1
 
     curr_t = t_start
-
-    weight = np.zeros(n_steps-1)
-    weight[0] = learn['eps']
-
-    ys = np.zeros((n_steps,3))
+    weight = learn['eps']
 
     last_spike = float("-inf")
+    last_spike_dendr = float("-inf")
 
     g_E_D = 0.0
-    gs = np.zeros(n_steps)
-    gs[0] = g_E_D
-
     syn_pots_sum = 0.0
-    curr_write_index = 1
 
-    while curr_t < t_end - 1.5*dt:
+    y = np.zeros(3)
 
-        g_E_D = g_E_D + np.sum(np.abs(pre_spikes-curr_t)<1e-10)*weight[curr_write_index-1]
+    vals = {'g':0.0,
+            'syn_pots_sum':0.0,
+            'y':y,
+            'spike':0.0,
+            'V_w_star':0.0,
+            'dendr_pred':0.0,
+            'h':0.0,
+            'dendr_spike':0.0,
+            'weight':weight}
+
+    accumulator.add(curr_t, **vals)
+
+    while curr_t < t_end - dt:
+
+        g_E_D = g_E_D + np.sum(np.abs(pre_spikes-curr_t)<1e-10)*weight
         g_E_D = g_E_D - dt*g_E_D/neuron['tau_s']
-
-        gs[curr_write_index] = g_E_D
 
         syn_pots_sum = np.sum(np.exp(-(curr_t - pre_spikes[pre_spikes <= curr_t])/neuron['tau_s']))
 
         args=(curr_t-last_spike, g_E_D, syn_pots_sum, I_ext(curr_t), neuron,)
-        ys[curr_write_index,:] = integrate.odeint(urb_senn_rhs, ys[curr_write_index-1,:], np.array([curr_t,curr_t+dt]),hmax=dt,args=args)[1,:]
+        y = integrate.odeint(urb_senn_rhs, y, np.array([curr_t,curr_t+dt]),hmax=dt,args=args)[1,:]
 
         if curr_t - last_spike < neuron['tau_ref']:
             does_spike = False
         else:
-            does_spike = spiker(ys[curr_write_index,:], curr_t, dt)
+            does_spike = spiker(y=y, curr_t=curr_t, dt=dt)
 
         if does_spike:
             last_spike = curr_t
 
-        V_w_star = neuron['g_D']/(neuron['g_D']+neuron['g_L'])*ys[curr_write_index,1]
+        V_w_star = neuron['g_D']/(neuron['g_D']+neuron['g_L'])*y[1]
         dendr_pred = phi(V_w_star, phi_params)
         h = phi_prime(V_w_star, phi_params)/phi(V_w_star, phi_params)
 
-        weight[curr_write_index] = weight[curr_write_index-1] + learn['eta']*(float(last_spike==curr_t) - dt*dendr_pred)*h*ys[curr_write_index,2]
-        
+        dendr_spike = spiker_dendr(y=y, curr_t=curr_t, last_spike=last_spike, last_spike_dendr=last_spike_dendr)
 
-        if weight[curr_write_index] < 0.0:
-            weight[curr_write_index] = 0.0
+        if dendr_spike:
+            last_spike_dendr = curr_t
+
+        weight += learn['eta']*(float(dendr_spike) - dt*dendr_pred)*h*y[2]
+
+        if weight < 0.0:
+            weight = 0.0
 
         curr_t = curr_t + dt
-        curr_write_index = curr_write_index + 1
 
-    return ys, weight, gs
+        vals = {'g':g_E_D,
+                'syn_pots_sum':syn_pots_sum,
+                'y':y,
+                'spike':float(does_spike),
+                'V_w_star':V_w_star,
+                'dendr_pred':dendr_pred,
+                'h':h,
+                'dendr_spike':float(dendr_spike),
+                'weight':weight}
+
+        accumulator.add(curr_t, **vals)
+
+    return accumulator
--- util.py
+++ util.py
@@ -6,10 +6,48 @@ def get_default(params):
     return json.load(open('default_{0}.json'.format(params),'r'))
 
 def fixed_spiker(spikes):
-    return lambda ys, curr_t, dt: np.min(np.abs(curr_t-spikes)) < dt/2
+    return lambda curr_t, dt, **kwargs: np.min(np.abs(curr_t-spikes)) < dt/2
 
 def phi_spiker(phi_params=None):
     if phi_params is None:
         phi_params = get_default_phi()
 
-    return lambda ys, curr_t, dt: phi(ys[0])*dt <= np.random.rand()
+    return lambda y, dt, **kwargs: phi(y[0])*dt <= np.random.rand()
+
+def inst_backprop(curr_t, last_spike, **kwargs):
+    return curr_t==last_spike
+
+def dendr_spike_det(thresh=1.0, tau=4.0):
+    return lambda y, curr_t, last_spike_dendr, **kwargs: y[1] > thresh and (curr_t-last_spike_dendr > tau)
+
+
+class Accumulator():
+
+    def _get_size(self, key):
+        if key=='y':
+            return 3
+        else:
+            return 1
+
+    def __init__(self, keys, sim, interval=1):
+        self.keys = keys
+        self.i = 0
+        self.j = 0
+        self.res = {}
+        self.interval = interval
+        eff_steps = (sim['end']-sim['start'])/(sim['dt']*interval) + 1
+        self.t = np.zeros(eff_steps)
+        for key in keys:
+            self.res[key] = np.zeros((eff_steps,self._get_size(key)))
+
+    def add(self, curr_t, **vals):
+        self.i += 1
+        if self.i == self.interval:
+            for key in self.keys:
+                self.res[key][self.j,:] = np.atleast_2d(vals[key])
+
+            self.t[self.j] = curr_t
+            self.j += 1
+            self.i = 0
+        else:
+            self.i += 1
