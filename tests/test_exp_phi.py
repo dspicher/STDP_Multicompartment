046ad214984db1db6aee313cdfa8006b3409a717
dominik <spicher@pyl.unibe.ch>
2015-01-21 16:00:03 +0100

    test changes

--- tests/test_exp_phi.py
+++ tests/test_exp_phi.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append("C:\\Users\\dominik\\Dropbox\\scholar\\phd\\stdp_modeling\\py_stdp_match\\repo")
 
-from util import phi_spiker, get_inst_backprop, periodic_current, get_dendr_spike_det, get_all_save_keys
+from util import get_phi_spiker, get_inst_backprop, get_periodic_current, get_dendr_spike_det, get_all_save_keys
 from helper import do, PeriodicAccumulator, BooleanAccumulator, dump
 import numpy as np
 from IPython import embed
@@ -9,26 +9,24 @@ import cPickle
 from collections import OrderedDict
 from simulation import run
 import matplotlib.pyplot as plt
+import time
 
 def test():
     spikes = np.array([])
 
-    for i in range(20):
-        print i
+    for i in range(10):
         my_s = {
-            'start': 30.0,
-            'end': 80.0,
+            'start': 0.0,
+            'end': 550.0,
             'dt': 0.05,
             'pre_spikes': np.array([]),
-            'I_ext': periodic_current(50.0, 100.0, 0.6, 50.0)
+            'I_ext': get_periodic_current(10.0, 100.0, 0.8, 90.0)
             }
 
         accs = [PeriodicAccumulator(get_all_save_keys(), my_s)]
 
-        acc = run(my_s, phi_spiker(), get_inst_backprop(), accs, seed=np.random.randint(100))[0]
+        acc = run(my_s, get_phi_spiker(), get_inst_backprop(), accs, seed=int(time.time()))[0]
 
-        spikes = np.concatenate((spikes, acc.t[np.squeeze(acc.res['spike'])>0.1]))
+        print acc.t[np.squeeze(acc.res['spike'])>0.1]
 
-        print spikes
-
-    assert np.isclose(np.min(spikes),np.max(spikes))
+test()
