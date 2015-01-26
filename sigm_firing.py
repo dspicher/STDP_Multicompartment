0ce4f76bf049410e995504e685b18169200d8174
dominik <spicher@pyl.unibe.ch>
2015-01-26 12:12:53 +0100

    new I params

--- sigm_firing.py
+++ sigm_firing.py
@@ -11,7 +11,7 @@ import time
 
 
 def task((repetition_i,p)):
-    
+
 
     neuron = get_default("sigm_neuron")
     neuron["phi"]["r_max"] = p["r0"]
@@ -24,27 +24,27 @@ def task((repetition_i,p)):
         'end': 2000.0,
         'dt': 0.05,
         'pre_spikes': np.array([]),
-        'I_ext': lambda t: get_periodic_current(249.25, 250.0, 1.5, 80.0)(t) + get_periodic_current(250.5, 250.0, 1.0, 50.0)(t)
-        }
-    
-    
+        'I_ext': lambda t: get_periodic_current(249.5, 250.0, 1.0, 80.0)(t)
+    }
+
+
     spiker = get_phi_spiker(neuron)
-    
+
     accs = [PeriodicAccumulator(get_all_save_keys(), interval=1), BooleanAccumulator(['spike', 'dendr_spike', 'pre_spike'])]
-        
+
     accums = run(my_s, get_phi_spiker(neuron), get_inst_backprop(), accs, seed=int(time.time()*1e6%1e8), neuron=neuron)
-    
+
     print p['ident']
     print accums[1].res['spike']
-        
+
     dump(accums,p['ident'])
 
 params = OrderedDict()
-params['r0'] = [1.0]
-params['k'] = [10]
-params['beta'] = [30.0]
-params['thresh'] = [30]
+params['r0'] = [1.5]
+params['k'] = [1.0]
+params['beta'] = [20.0]
+params['thresh'] = [20.0]
 
 file_prefix = 'sigm_firing'
 
-do(task, params, file_prefix, prompt=False, withmp=True)
+do(task, params, file_prefix, prompt=False, withmp=False)
