def8c6514865de7478f800dd60dfb4f7115aff11
Dominik Spicher <spicher@pyl.unibe.ch>
2015-02-11 18:07:36 +0100

    modified run

--- artola.py
+++ artola.py
@@ -1,5 +1,5 @@
 
-from util import get_all_save_keys, get_periodic_current, get_inst_backprop, get_phi_spiker, get_dendr_spike_det
+from util import get_all_save_keys, get_periodic_current, get_inst_backprop, get_phi_spiker, get_dendr_spike_det_dyn_ref
 from helper import do, PeriodicAccumulator, BooleanAccumulator, dump, get_default
 import numpy as np
 from IPython import embed
@@ -15,28 +15,30 @@ def task((repetition_i,p)):
     neuron = get_default("sigm_neuron")
     neuron["phi"]['alpha'] = p["alpha"]
     neuron["phi"]['beta'] = p["beta"]
+    neuron["phi"]['r_max'] = p["r_max"]
 
     my_s = {
         'start': 0.0,
-        'end': 500.0,
+        'end': 300.0,
         'dt': 0.05,
-        'pre_spikes': np.linspace(0.0,500.0,p["n_spikes"]+2)[1:-1],
+        'pre_spikes': np.array([50.0]),
         'I_ext': lambda t:0.0
         }
 
     seed = int(int(time.time()*1e8)%1e9)
-    accs = [PeriodicAccumulator(get_all_save_keys(), interval=1)]
-    accums = run(my_s, lambda **kwargs:False, get_dendr_spike_det(thresh=p['thresh'],tau_ref=p['tau_ref']), accs, seed=seed, neuron=neuron, voltage_clamp=True, U_clamp=p['Uclamp'])
+    accs = [PeriodicAccumulator(['weight'], interval=1)]
+    accums = run(my_s, lambda **kwargs:False, get_dendr_spike_det_dyn_ref(p["thresh"], p["tau_ref_0"], p["theta_0"]), accs, seed=seed, neuron=neuron, voltage_clamp=True, U_clamp=p['Uclamp'])
 
     dump(accums,p['ident'])
 
 params = OrderedDict()
-params["alpha"] = [-40]
-params["beta"] = np.linspace(0.15,0.4,6)
-params["n_spikes"] = [1,5,10]
-params["thresh"] = [-40,-30,-20]
-params["tau_ref"] = [2.0,5.0,10.0,20.0]
-params["Uclamp"] = np.linspace(-70,0,8)
+params["alpha"] = np.linspace(-30,-40,6)
+params["beta"] = np.linspace(0.1,0.4,7)
+params["r_max"] = [0.05,0.1,0.15]
+params["thresh"] = [50]
+params["tau_ref_0"] = [5.0]
+params["theta_0"] = [2.5]
+params["Uclamp"] = [-40.0,-30.0,-20.0]
 
 file_prefix = 'artola'
 
