4dfce079022b4ec5b0c1993b993ca5d7eba855f3
Dominik Spicher <spicher@pyl.unibe.ch>
2015-02-12 16:24:43 +0100

    stdp success

--- nice_stdp_1.py
+++ nice_stdp_1.py
@@ -1,4 +1,3 @@
-
 from util import get_all_save_keys, get_periodic_current, get_inst_backprop, get_phi_spiker, get_dendr_spike_det_dyn_ref, get_fixed_spiker
 from helper import do, PeriodicAccumulator, BooleanAccumulator, dump, get_default
 import numpy as np
@@ -9,12 +8,11 @@ from simulation import run
 import matplotlib.pyplot as plt
 import time
 
-
 def task((repetition_i,p)):
 
     learn = {}
-    learn['eta'] = 1e-3*p["eps"]
-    learn['eps'] = p["eps"]
+    learn['eta'] = 2.6e-6 # 40 spikes at 1e-7
+    learn['eps'] = 1e-3
     learn['tau_delta'] = 2.0
 
     neuron = get_default("neuron")
@@ -35,21 +33,17 @@ def task((repetition_i,p)):
     
     seed = int(int(time.time()*1e8)%1e9)
     accs = [PeriodicAccumulator(get_all_save_keys(), interval=10), BooleanAccumulator(['spike', 'dendr_spike', 'pre_spike'])]
-    accums = run(my_s, get_fixed_spiker(spikes), get_dendr_spike_det_dyn_ref(p["thresh"],p["tau_ref_0"],p["theta_0"]), accs, seed=seed, learn=learn, neuron=neuron)
+    accums = run(my_s, get_fixed_spiker(spikes), get_dendr_spike_det_dyn_ref(-50.0,10.0,100.0), accs, seed=seed, learn=learn, neuron=neuron)
         
 
     dump(accums,p['ident'])
 
 params = OrderedDict()
-params["alpha"] = np.linspace(-60.0,-50.0,3)
-params["beta"] = np.linspace(0.2,0.7,6)
-params["r_max"] = [0.05, 0.1, 0.2, 0.5]
-params["thresh"]=[-50]
-params["tau_ref_0"]=[10]
-params["theta_0"]=[100]
-params["eps"] = [1e-3,5e-3]
-params["delta"] = [-30,-20,-10,10,20,30]
+params["alpha"] = [-59]
+params["beta"] = [0.5]
+params["r_max"] = [ 0.15    ]
+params["delta"] = np.linspace(-80,80,81)
 
-file_prefix = 'single_shot_stdp_phi_sigm'
+file_prefix = 'nice_stdp_1'
 
 do(task, params, file_prefix, prompt=False, withmp=True)
