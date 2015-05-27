507fcd749e43a184ad0d9fd16e9829c82e11a77b
Dominik Spicher <spicher@pyl.unibe.ch>
2015-05-27 09:16:51 +0200

    removed dynamic refractory period from sjostrom switch

--- stdp_figure_sjostrom_switch.py
+++ stdp_figure_sjostrom_switch.py
@@ -21,7 +21,7 @@ simulation results next to each other.
 """
 
 
-from util import get_all_save_keys, get_periodic_current, get_inst_backprop, get_phi_spiker, get_dendr_spike_det_dyn_ref, get_fixed_spiker
+from util import get_all_save_keys, get_periodic_current, get_inst_backprop, get_phi_spiker, get_dendr_spike_det, get_fixed_spiker
 from helper import do, PeriodicAccumulator, BooleanAccumulator, dump, get_default
 import numpy as np
 from IPython import embed
@@ -35,37 +35,36 @@ import time
 def task((repetition_i,p)):
 
     learn = get_default("learn")
-    learn["eta"] = 1e-7
+    learn["eta"] = 3e-7
 
     neuron = get_default("neuron")
-    neuron["phi"]["alpha"] = -58.0
+    neuron["phi"]["alpha"] = -52.0
     neuron["phi"]["beta"] = 0.25
-    neuron["phi"]["r_max"] = p["r_max"]
+    neuron["phi"]["r_max"] = 0.35
 
 
-    spikes = np.arange(20.0,1600.0,100.0)
+    spikes = np.arange(20.0,301.0,20.0)
 
     my_s = {
         'start': 0.0,
-        'end': 1550.0,
+        'end': 350.0,
         'dt': 0.05,
         'pre_spikes': [spikes-10.0],
         'I_ext': lambda t: 0.0
         }
 
-    # 0.4 <= p <= 0.8
-    prob = 0.4*np.random.rand()+0.4
+    # 0.2 <= p <= 0.55
+    prob = 0.35*np.random.rand()+0.2
 
     seed = int(int(time.time()*1e8)%1e9)
-    accs = [PeriodicAccumulator(get_all_save_keys(), interval=10)]
-    accums = run(my_s, get_fixed_spiker(spikes), get_dendr_spike_det_dyn_ref(-55.0,20.0,1.0), accs, neuron=neuron, seed=seed, learn=learn, p_backprop=prob)
+    accs = [PeriodicAccumulator(['weights'], interval=10)]
+    accums = run(my_s, get_fixed_spiker(spikes), get_dendr_spike_det(-55.0), accs, neuron=neuron, seed=seed, learn=learn, p_backprop=prob)
 
 
     dump((prob,accums),p['ident'])
 
 params = OrderedDict()
 params["i"] = range(100)
-params["r_max"] = [0.35]
 
 file_prefix = 'stdp_figure_sjostrom_switch'
 
