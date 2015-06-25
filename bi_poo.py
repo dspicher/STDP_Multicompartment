713cf25510c2e4eb7a121be675c8c47226ccf753
Dominik Spicher <spicher@pyl.unibe.ch>
2015-06-25 14:20:27 +0200

    ...

--- bi_poo.py
+++ bi_poo.py
@@ -35,22 +35,21 @@ import os
 
 def task((repetition_i,p)):
 
-    learn = {}
-    learn['eta'] = 1e-6
-    learn['eps'] = 1e-3
-    learn['tau_delta'] = 2.0
-
     neuron = get_default("neuron")
     neuron["phi"]['r_max'] = p["r_max"]
     neuron["phi"]['alpha'] = p["alpha"]
     neuron["phi"]['beta'] = p["beta"]
-    neuron["tau_s"] = p["tau_s"]
+    neuron["g_L"] = p["g_L"]
 
-    spikes = np.array([101.0])
+    learn = get_default("learn")
+    learn["eps"] = learn["eps"]*p["l_f"]
+    learn["eta"] = learn["eta"]*p["l_f"]
+
+    spikes = np.array([71.0])
 
     my_s = {
         'start': 0.0,
-        'end': 300.0,
+        'end': 200.0,
         'dt': 0.05,
         'pre_spikes': [spikes + p["delta"]],
         'I_ext': lambda t: 0.0
@@ -58,17 +57,20 @@ def task((repetition_i,p)):
 
 
     seed = int(int(time.time()*1e8)%1e9)
-    accs = [PeriodicAccumulator(get_all_save_keys(), interval=10), BooleanAccumulator(['spike', 'dendr_spike', 'pre_spikes'])]
-    accums = run(my_s, get_fixed_spiker(spikes), get_dendr_spike_det(-50.0,10.0), accs, seed=seed, learn=learn, neuron=neuron, h=1.0)
+    accs = [PeriodicAccumulator(['y','weights'], interval=10), BooleanAccumulator(['spike', 'dendr_spike', 'pre_spikes'])]
+    accums = run(my_s, get_fixed_spiker(spikes), get_dendr_spike_det(-50.0,10.0), accs, seed=seed, neuron=neuron, learn=learn, h=1.0)
 
 
     dump(accums,'bi_poo/'+p['ident'])
 
 params = OrderedDict()
-params["alpha"] = [-53.0,-55.0,-57.0,]
-params["beta"] = [0.3,0.35,0.4]
-params["r_max"] = [0.25,0.3,0.35]
-params["tau_s"] = [2.0, 4.0]
-params["delta"] = np.linspace(-100,100,51)
+params["g_L"] = [0.05, 0.066, 0.075]
+params["alpha"] = [-54.0,-55.0,-56.0]
+params["beta"] = [0.37,0.4,0.43]
+params["r_max"] = [0.3,0.35,0.4]
+params["l_f"] = [0.1,1.0,10.0]
+params["delta"] = [-70.0,-50.0,-30.0,-10.0,-5.0,-2.0,0.0,2.0,5.0,10.0,30.0,50.0,70.0]
+
+file_prefix = 'bi_poo'
 
 do(task, params, file_prefix, create_notebooks=True)
