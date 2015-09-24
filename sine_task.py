da0c57019401a7f79322c6324ff7a6a7687d8686
Dominik Spicher <spicher@pyl.unibe.ch>
2015-09-24 12:05:15 +0200

    did first version of task, most simplified setup but works great

---
+++ sine_task.py
@@ -0,0 +1,93 @@
+
+import cPickle
+import time
+from collections import OrderedDict
+
+import matplotlib.pyplot as plt
+import numpy as np
+from IPython import embed
+
+from helper import (BooleanAccumulator, PeriodicAccumulator, do, dump,
+                    get_default)
+from simulation import run
+from util import (get_all_save_keys, get_fixed_spiker, get_inst_backprop,
+                  get_periodic_current, get_phi_spiker, get_phi_U_learner)
+
+
+def task((repetition_i, p)):
+
+    n_syn = p["n_syn"]
+
+    learn = get_default("learn")
+    learn["eps"] = 1e-1 / n_syn
+    learn["eta"] = 1e-3 / n_syn
+
+    neuron = get_default("neuron")
+    neuron["phi"]["alpha"] = p["alpha"]
+    neuron["phi"]["beta"] = p["beta"]
+    neuron["phi"]["r_max"] = p["r_max"]
+    neuron["g_S"] = p["g_S"]
+
+    epochs = 4
+    l_c = 6
+    eval_c = 2
+    cycles = epochs * l_c + (epochs + 1) * eval_c
+    cycle_dur = p["cycle_dur"]
+    t_end = cycles * cycle_dur
+
+    def exc_soma_cond(t):
+        if t % (cycle_dur * (l_c + eval_c)) < cycle_dur * eval_c:
+            return 0.0
+        else:
+            return ((1 + np.sin(np.pi / 2 + t / t_end * cycles * 2 * np.pi)) * 2e-3 * 1 + 8e-3) * p["g_factor"]
+
+    def inh_soma_cond(t):
+        if t % (cycle_dur * (l_c + eval_c)) < cycle_dur * eval_c:
+            return 0.0
+        else:
+            return 8e-2 * p["g_factor"]
+
+    rhythmic_spikes = [np.arange(1.0 * i / n_syn * cycle_dur, t_end, cycle_dur)
+                       for i in range(n_syn)]
+
+
+    dt = 0.05
+    f_r = 0.01  # 10Hz
+    t_pts = np.arange(0, t_end / cycles, dt)
+
+    poisson_spikes = [t_pts[np.random.rand(t_pts.shape[0]) < f_r * dt] for _ in range(n_syn)]
+    poisson_spikes = [[] if spikes.shape[0] == 0 else np.concatenate(
+        [np.arange(spike, t_end, cycle_dur) for spike in spikes]) for spikes in poisson_spikes]
+    for train in poisson_spikes:
+        train.sort()
+
+    embed()
+
+    my_s = {
+        'start': 0.0,
+        'end': t_end,
+        'dt': dt,
+        'pre_spikes': poisson_spikes,
+        'syn_cond_soma': {'E': exc_soma_cond, 'I': inh_soma_cond},
+        'I_ext': lambda t: 0.0
+    }
+
+    seed = int(int(time.time() * 1e8) % 1e9)
+    accs = [PeriodicAccumulator(get_all_save_keys(), interval=40)]
+    accums = run(my_s, get_fixed_spiker(np.array([])), get_phi_U_learner(neuron, dt),
+                 accs, neuron=neuron, seed=seed, learn=learn)
+
+    dump((seed, accums), 'sine_task/' + p['ident'])
+
+params = OrderedDict()
+params["n_syn"] = [100, 200]
+params["g_factor"] = [10]
+params["cycle_dur"] = [100, 200]
+params["g_S"] = [0.0, 0.5]
+params["alpha"] = [-50.0, -55.0]
+params["beta"] = [0.2, 0.25]
+params["r_max"] = [0.25, 0.35]
+
+file_prefix = 'sine_task'
+
+do(task, params, file_prefix, withmp=False, create_notebooks=True)
